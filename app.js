13e35386aa3bc2e8e1c926a6c9a8abc886a279fc
JaeHyeon Son <sjh9391985@gmail.com>
2021-06-26 16:15:40 +0900

    Using JavaScript function

--- app.js
+++ app.js
@@ -13,7 +13,56 @@
 //const title = document.getElementsByTagName("h1");
 //console.log(title);
 
-const title = document.querySelector("h1");
-                        //-> 여러개의 쿼리(배열형식)를 가져오고 싶다면 querySelectorAll()을 사용할것.
-console.log(title);
+/* -> listener 사용법
+const title = document.querySelector("div.hello:first-child h1");
+                      //-> 여러개의 쿼리(배열형식)를 가져오고 싶다면 querySelectorAll()을 사용할것.
 
+function handleTitlcClick() {
+    console.log("title was clicked");
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here";
+}
+function handleMouseLeave() {
+    title.innerText = "Moust is gone";
+}
+function handleWindowResize() {
+    document.body.style.background = "tomato";  
+    document.body.style.color = "red";
+}
+title.addEventListener("click", handleTitlcClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+window.addEventListener("resize", handleWindowResize);
+*/
+
+
+const h1 = document.querySelector("div.hello:first-child h1");
+// Javascript에서는 css처리를 하는것이 효율적이지는 않다.
+/*function handleTitleClick() {
+    if(h1.style.color === "blue"){
+        h1.style.color = "tomato";    
+    }else{
+        h1.style.color = "blue";
+    }
+    console.log(h1.style.color); 
+}
+h1.addEventListener("click",handleTitleClick);*/
+
+// classList 사용하기
+/*function handleTitleClick() {
+    const clickedName = "active";
+    if(h1.classList.contains(clickedName)){  // === h1.className === clickedName 
+        h1.classList.remove(clickedName)  //h1.className = ""
+    }else{
+        h1.classList.add(clickedName)  //h1.className = clickedName;
+    }
+}
+h1.addEventListener("click", handleTitleClick);*/
+
+// toggle 사용하기
+function handleTitleClick() {
+    h1.classList.toggle("active"); // h1 태크의 classlist에서 active 클래스가 있는지 확인하고 있다면 제거, 없다면 생성을 해주는 역할을 하는 함수.
+}
+h1.addEventListener("click", handleTitleClick);
